2d5ff0b643842e8f4a8af37883abac283f1d4023
Falak Tulsi <falaktulsi@gmail.com>
2023-05-25 16:22:17 -0700

    Fixed .env loading errors

--- main.go
+++ main.go
@@ -2,27 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	envErr := godotenv.Load(".env")
+
+	if envErr != nil {
+		fmt.Println(envErr)
+		log.Fatalf("Error loading .env file")
+	}
 
 	bot, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
+		log.Fatalf("Error making bot session")
 	}
 
 	err = bot.Open()
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
+		log.Fatalf("Error opening bot: ")
 	}
-
 	fmt.Println("Bot is now running!")
 
 	sc := make(chan os.Signal, 1)
